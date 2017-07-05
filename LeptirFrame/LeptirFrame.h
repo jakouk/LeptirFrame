783e1b720b27584c6cd932ef0892ec2d7f0049e6
jakouk <jakoriaty@gmail.com>
2017-07-05 20:16:38 +0900

    Initial commit

---
+++ LeptirFrame/LeptirFrame.h
@@ -0,0 +1,19 @@
+//
+//  LeptirFrame.h
+//  LeptirFrame
+//
+//  Created by jakouk on 2017. 7. 3..
+//  Copyright © 2017년 jakouk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LeptirFrame.
+FOUNDATION_EXPORT double LeptirFrameVersionNumber;
+
+//! Project version string for LeptirFrame.
+FOUNDATION_EXPORT const unsigned char LeptirFrameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeptirFrame/PublicHeader.h>
+
+
---
+++ LeptirFrameTests/LeptirFrameTests.swift
@@ -0,0 +1,76 @@
+//
+//  LeptirFrameTests.swift
+//  LeptirFrameTests
+//
+//  Created by jakouk on 2017. 7. 3..
+//  Copyright © 2017년 jakouk. All rights reserved.
+//
+
+import XCTest
+@testable import LeptirFrame
+
+class LeptirFrameTests: XCTestCase {
+  
+  
+  func testViewLeft_get() {
+    let view = UIView()
+    view.frame.origin.x = 150
+    XCTAssertEqual(view.left, 150)
+  }
+  
+  func testViewLeft_set() {
+    let view = UIView()
+    view.left = 100
+    XCTAssertEqual(view.frame.origin.x, 100)
+  }
+  
+  func testViewTop_get() {
+    let view = UIView()
+    view.frame.origin.y = 200
+    XCTAssertEqual(view.top, 200)
+  }
+  
+  func testViewTop_set() {
+    let view = UIView()
+    view.top = 50
+    XCTAssertEqual(view.frame.origin.y, 50)
+  }
+  
+  // 미션: 
+  // 아래 두 테스트 케이스가 통과되도록 만들어보세요.
+  
+  func testViewRight_get() {
+    let view = UIView()
+    view.width = 100
+    view.height = 50
+    view.frame.origin.x = 70
+    XCTAssertEqual(view.right, 100+70)
+    
+  }
+  
+  func testViewRight_set() {
+    let view = UIView()
+    view.width = 100
+    view.height = 50
+    view.right = 130
+    XCTAssertEqual(view.frame.origin.x, 130-100)
+    
+  }
+  
+  func testViewBottom_get() {
+    let view = UIView()
+    view.width = 100
+    view.height = 50
+    view.frame.origin.y = 70
+    XCTAssertEqual(view.bottom, 50 + 70 )
+  }
+  
+  func testViewBottom_set() {
+    let view = UIView()
+    view.width = 100;
+    view.height = 50
+    view.bottom = 130
+    XCTAssertEqual(view.frame.origin.y, 130 - 50)
+  }
+  
+}
